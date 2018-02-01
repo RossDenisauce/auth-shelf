5d2995cae6c89df8a763100f456e351a2a4154fd
Andrew Johnson <ajj@byu.net>
2018-02-01 12:30:25 -0600

    Added shelf controller to user and home

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,6 +1,11 @@
-myApp.controller('UserController', ['UserService', function(UserService) {
+myApp.controller('UserController', ['UserService', 'ShelfService', function(UserService, ShelfService) {
   console.log('UserController created');
   var self = this;
   self.userService = UserService;
   self.userObject = UserService.userObject;
+
+  self.shelf = ShelfService.shelf;
+  self.getShelf = ShelfService.getShelf;
+  self.getShelf();
+
 }]);
