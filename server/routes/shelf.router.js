295ab0507368ce24df16b824b009def113680ed1
Andrew Johnson <ajj@byu.net>
2018-02-01 13:32:11 -0600

    Fixed item query

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -4,11 +4,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Person = require('../models/Person').Person;
-const Item = require('../models/Person').Item;
 
 router.get('/', (req, res) => {
     console.log('hit shelf route');
-    Item.find({}, function(error, response) {
+    Person.find({}, function(error, response) {
         if(error) {
             res.sendStatus(500);
         } else {
