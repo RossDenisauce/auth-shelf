e3275a3861d1317e9a3bc4fcec2d2a2d0d22852e
Paul Tiller <pftiller@gmail.com>
2018-02-01 11:10:59 -0600

    Updating the database file

--- server/models/Person.js
+++ server/models/Person.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Mongoose Schema
+
 const PersonSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
 });
+
+
+const ItemsSchema = new Schema({
+  description: {type: String, required: true},
+  placer: [PersonSchema],
+  image_url:  String
+})
+
+
 
 module.exports = mongoose.model('Person', PersonSchema);
